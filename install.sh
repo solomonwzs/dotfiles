bd8e040def2ad7b7b4380d6a9dd9f5e3fdfdd42f
solomon <solomon.wzs@gmail.com>
2020-05-09 20:20:17 +0800

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -10,11 +10,23 @@ set -euo pipefail
 EXECUTE_FILENAME=$(readlink -f "$0")
 EXECUTE_DIRNAME=$(dirname "$EXECUTE_FILENAME")
 
+function message() {
+    echo -e "\033[01;32m[INFO]\033[01;37m $1\033[0m"
+}
+
 function make_link() {
     target="$1"
     link_name="$2"
-    ([ -e "$link_name" ] && echo "$link_name has exists") || \
+    message "Create '$link_name'"
+    ([ -e "$link_name" ] && message "'$link_name' has exists") || \
         ln -s "$target" "$link_name"
+}
+
+function git_clone() {
+    addr="$1"
+    dir="$2"
+    ([ -e "$dir" ] && message "'$dir' has exists" ) || \
+        git clone "$addr" "$dir"
 }
 
 make_link "$EXECUTE_DIRNAME/vim" "$HOME/.vim"
@@ -23,8 +35,20 @@ make_link "$EXECUTE_DIRNAME/vim/vimrc" "$HOME/.config/nvim/init.vim"
 
 make_link "$EXECUTE_DIRNAME/zsh/zshrc" "$HOME/.zshrc"
 
+message "Create terminfo"
 [ -e "$HOME/.terminfo/t/tmux-256color" ] || \
     tic -x "$EXECUTE_DIRNAME/tmux/tmux-256color.terminfo"
 [ -e "$HOME/.terminfo/x/xterm-256color-italic" ] || \
     tic -x "$EXECUTE_DIRNAME/tmux/xterm-256color-italic.terminfo"
+
 make_link "$EXECUTE_DIRNAME/tmux/tmux.conf" "$HOME/.tmux.conf"
+
+OH_MY_ZSH_PATH="$HOME/.oh-my-zsh"
+message "Download 'oh-my-zsh'"
+git_clone "https://github.com/ohmyzsh/ohmyzsh.git" "$OH_MY_ZSH_PATH"
+message "Download 'zsh-autosuggestions'"
+git_clone "https://github.com/zsh-users/zsh-autosuggestions.git" \
+    "$OH_MY_ZSH_PATH/custom/plugins/zsh-autosuggestions"
+message "Download 'zsh-syntax-highlighting'"
+git_clone "https://github.com/zsh-users/zsh-syntax-highlighting.git" \
+    "$OH_MY_ZSH_PATH/custom/plugins/zsh-syntax-highlighting"
