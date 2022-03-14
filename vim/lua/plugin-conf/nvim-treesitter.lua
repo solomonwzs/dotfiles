5467a3c1013df4dc61b9c062ab8c53804ed7da08
solomon[credential] <solomon.wzs@gmail.com>
2022-03-14 14:51:07 +0800

    modify 	modified:   vim/lua/plugin-conf/nvim-treesitter.lua

--- vim/lua/plugin-conf/nvim-treesitter.lua
+++ vim/lua/plugin-conf/nvim-treesitter.lua
@@ -10,7 +10,7 @@ require("nvim-treesitter.configs").setup {
 
   highlight = {
     -- `false` will disable the whole extension
-    enable = true,
+    enable = false,
 
     -- list of language that will be disabled
     -- disable = {"c", "rust"},
